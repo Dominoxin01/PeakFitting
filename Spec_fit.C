bc3d8414d16443f4b5b70559e29ff1d5f12d24d1
Dominik Stajkowski <d.stajkowski.1@pgr.bham.ac.uk>
2023-12-11 16:10:46 +0000

    Added the abillity to fit 2, 3 & 4 gussians with a linear background by addin a switch. This code needs testing and clearing up

--- Spec_fit.C
+++ Spec_fit.C
@@ -30,24 +30,151 @@ void SpecLoad(const char *filename){
 void FitData(){
 //This function fits a function with a linear background and a Gussian peak, fit1g for one gaussian.
 	int low_x, up_x;
+	int number_peaks;
+	int FWHM_est;
+	int cen_1, cen_2, cen_3, cen_4;
 
+	std::cout << "How many peaks: 1, 2, 3 or 4?" << std::endl;
+	std::cin >> number_peaks; //taking user input
+	 
 	//asking the user for the lower and upper fit boundires:
 	std::cout << "Lower channel number for the fit boundry:" << std::endl;
 	std::cin >> low_x ; //taking user input
 	std::cout << "Upper channel number for the fit boundry:" << std::endl;
 	std::cin >> up_x ; //taking user input
+	
+	std::cout << "FWHM estimate:" << std::endl;
+	std::cin >> FWHM_est;
+	
 	h1->GetXaxis()->SetRangeUser(low_x, up_x);
 	
-	TF1 *f1 = new TF1("f1", "gausn(0) + pol1(3)", low_x, up_x); // fit1g last 5
-	f1->SetParNames("Counts", "Centroid" ,"Sigma","Intercept","Slope");
-    	f1->SetParLimits(1, low_x, up_x);
-    	f1->SetParameter(1, 0.5*(up_x-low_x)+low_x);
-    	f1->SetParLimits(2,0., up_x-low_x);
-    	f1->SetParameter(2, 0.5*(up_x-low_x)/2.35);
-    	std::cout << up_x-low_x << std::endl;
-    	//f1->SetParLimits(1,0., 1000000000);
-    	f1->SetParLimits(0, 0., 1000000000000);
-    	f1->SetParameter(0, h1->GetBinContent(int(0.5*(up_x-low_x))));
+	switch (number_peaks){
+	case 1: {//setting the fitting function as a scaled normalised Gaussian and a linear background:
+		TF1 *f1 = new TF1("f1", "gausn(0) + pol1(3)", low_x, up_x); // fit1g last 5
+		f1->SetParNames("Counts", "Centroid" ,"Sigma","Intercept","Slope");
+		//setting limits on the counts parameter:
+		f1->SetParLimits(0, 0., 1000000000000);
+		//giving an initial guess on the counts:
+    		f1->SetParameter(0, h1->GetBinContent(int(0.5*(up_x-low_x))));
+    		//assuming that the centroid will be within the selected region:
+    		f1->SetParLimits(1, low_x, up_x);
+    		//assumming that the centroid is in the middle of the selected region:
+    		f1->SetParameter(1, 0.5*(up_x-low_x)+low_x);
+    		//the standard deviation should be less than the selected region:
+    		f1->SetParLimits(2,0., up_x-low_x);
+    		//assuming that the standard deviation is half the selected width (may be sub-optimal:)
+    		f1->SetParameter(2, 0.5*(up_x-low_x)/2.35);
+    		//std::cout << up_x-low_x << std::endl;
+		break;
+		}
+		
+	case 2: {//setting the fitting function as two scaled normalised Gaussians and a linear background:
+		TF1 *f2 = new TF1("f2", "gausn(0) + gausn(3) + pol1(6)", low_x, up_x);
+		f2->SetParNames("1 Counts", "1 Centroid" ,"1 Sigma", "2 Counts", "2 Centroid" ,"2 Sigma","Intercept","Slope");
+		//Assuming tha both centroids are seen in the selected region:
+		f2->SetParLimits(1, low_x, up_x);
+		f2->SetParLimits(4, low_x, up_x);
+		// asking th user for estimated centroid values and taking the extimate of the scaling:
+		std::cout << "Centroid 1:" << std::endl;
+		std::cin >> cen_1;
+		f2->SetParameter(1, cen_1);
+		f2->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		std::cout << "Centroid 2:" << std::endl;
+		std::cin >> cen_2;
+		f2->SetParameter(4, cen_2);
+		f2->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		//setting up the fitting of the standard deviation:
+		f2->SetParLimits(2,0., up_x-low_x);
+		f2->SetParLimits(5,0., up_x-low_x);
+		f2->SetParameter(2, FWHM_est/2.35);
+		f2->SetParameter(5, FWHM_est/2.35);
+		break;
+		}
+	case 3: {
+		TF1 *f3 = new TF1("f3", "gausn(0) + gausn(3) + gausn(6) + pol1(9)", low_x, up_x);
+		f3->SetParNames("1 Counts", "1 Centroid" ,"1 Sigma", "2 Counts", "2 Centroid" ,"2 Sigma", "3 Counts", "3 Centroid" ,"3 Sigma", "Intercept","Slope");
+		//Assuming tha both centroids are seen in the selected region:
+		f3->SetParLimits(1, low_x, up_x);
+		f3->SetParLimits(4, low_x, up_x);
+		f3->SetParLimits(7, low_x, up_x);
+		// asking th user for estimated centroid values and taking the extimate of the scaling:
+		std::cout << "Centroid 1:" << std::endl;
+		std::cin >> cen_1;
+		f3->SetParameter(1, cen_1);
+		f3->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		std::cout << "Centroid 2:" << std::endl;
+		std::cin >> cen_2;
+		f3->SetParameter(4, cen_2);
+		f3->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		std::cout << "Centroid 3:" << std::endl;
+		std::cin >> cen_3;
+		f3->SetParameter(7, cen_3);
+		f3->SetParameter(6, h1->GetBinContent(int(cen_3)));
+		//setting up the fitting of the standard deviation:
+		f3->SetParLimits(2,0., up_x-low_x);
+		f3->SetParLimits(5,0., up_x-low_x);
+		f3->SetParLimits(8, 0., up_x-low_x);
+		f3->SetParameter(2, FWHM_est/2.35);
+		f3->SetParameter(5, FWHM_est/2.35);
+		f3->SetParameter(8, FWHM_est/2.35);
+		break;
+		}
+	case 4: {
+		TF1 *f4 = new TF1("f4", "gausn(0) + gausn(3) + gausn(6) + gausn(9) + pol1(12)", low_x, up_x);
+		f4->SetParName(0, "1 Counts");
+		f4->SetParName(1, "1 Centroid");
+		f4->SetParName(2 ,"1 Sigma");
+		f4->SetParName(3, "2 Counts");
+		f4->SetParName(4, "2 Centroid");
+		f4->SetParName(5 ,"2 Sigma");
+		f4->SetParName(6, "3 Counts");
+		f4->SetParName(7, "3 Centroid");
+		f4->SetParName(8 ,"3 Sigma");
+		f4->SetParName(9,  "4 Counts");
+		f4->SetParName(10, "4 Centroid");
+		f4->SetParName(11 , "4 Sigma");
+		f4->SetParName(12, "Intercept");
+		f4->SetParName(13, "Slope");
+		//Assuming tha both centroids are seen in the selected region:
+		f4->SetParLimits(1, low_x, up_x);
+		f4->SetParLimits(4, low_x, up_x);
+		f4->SetParLimits(7, low_x, up_x);
+		f4->SetParLimits(10, low_x, up_x);
+		// asking th user for estimated centroid values and taking the extimate of the scaling:
+		std::cout << "Centroid 1:" << std::endl;
+		std::cin >> cen_1;
+		f4->SetParameter(1, cen_1);
+		f4->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		std::cout << "Centroid 2:" << std::endl;
+		std::cin >> cen_2;
+		f4->SetParameter(4, cen_2);
+		f4->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		std::cout << "Centroid 3:" << std::endl;
+		std::cin >> cen_3;
+		f4->SetParameter(7, cen_3);
+		f4->SetParameter(6, h1->GetBinContent(int(cen_3)));
+		std::cout << "Centroid 4" << std::endl;
+		std::cin >> cen_4;
+		f4->SetParameter(10, cen_4);
+		f4->SetParameter(9, h1->GetBinContent(int(cen_4)));
+		//setting up the fitting of the standard deviation:
+		f4->SetParLimits(2,0., up_x-low_x);
+		f4->SetParLimits(5,0., up_x-low_x);
+		f4->SetParLimits(8, 0., up_x-low_x);
+		f4->SetParLimits(11, 0., up_x-low_x);
+		f4->SetParameter(2, FWHM_est/2.35);
+		f4->SetParameter(5, FWHM_est/2.35);
+		f4->SetParameter(8, FWHM_est/2.35);
+		f4->SetParameter(11, FWHM_est/2.35);
+	
+	
+		break;
+		}
+		
+	
+	}
+	
+	
 	h1->Fit("f1", "IM", "", low_x, up_x);
 	h1->Draw("Same");
 	
