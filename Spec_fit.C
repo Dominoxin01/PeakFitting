62604ee804e085ec5b3eb4991c1fd54abf8c84ea
Dominik Stajkowski <d.stajkowski.1@pgr.bham.ac.uk>
2023-12-12 17:46:18 +0000

    Added a Zoom function which asks for the channel boundries.

--- Spec_fit.C
+++ Spec_fit.C
@@ -55,7 +55,11 @@ void FitData(){
 	switch (number_peaks){
 	case 1: {//setting the fitting function as a scaled normalised Gaussian and a linear background:
 		TF1 *f1 = new TF1("f1", "gausn(0) + pol1(3)", low_x, up_x); // fit1g last 5
-		f1->SetParNames("Counts", "Centroid" ,"Sigma","Intercept","Slope");
+		f1->SetParName(0 , "1 Counts");
+		f1->SetParName(1 , "1 Centroid");
+		f1->SetParName(2 , "1 Sigma");
+		f1->SetParName(3 , "Intercept");
+		f1->SetParName(4 , "Slope");
 		//setting limits on the counts parameter:
 		f1->SetParLimits(0, 0., 1000000000000);
 		//giving an initial guess on the counts:
@@ -75,30 +79,50 @@ void FitData(){
 		
 	case 2: {//setting the fitting function as two scaled normalised Gaussians and a linear background:
 		TF1 *f2 = new TF1("f2", "gausn(0) + gausn(3) + pol1(6)", low_x, up_x);
-		f2->SetParNames("1 Counts", "1 Centroid" ,"1 Sigma", "2 Counts", "2 Centroid" ,"2 Sigma","Intercept","Slope");
+		f2->SetParName(0 , "1 Counts");
+		f2->SetParName(1 , "1 Centroid");
+		f2->SetParName(2 , "1 Sigma");
+		f2->SetParName(3 , "2 Counts");
+		f2->SetParName(4 , "2 Centroid");
+		f2->SetParName(5 , "2 Sigma");
+		f2->SetParName(6 , "Intercept");
+		f2->SetParName(7 , "Slope");
 		//Assuming tha both centroids are seen in the selected region:
 		f2->SetParLimits(1, low_x, up_x);
 		f2->SetParLimits(4, low_x, up_x);
-		// asking th user for estimated centroid values and taking the extimate of the scaling:
+		//asking th user for estimated centroid valuesand taking the extimate of the scaling:
 		std::cout << "Centroid 1:" << std::endl;
 		std::cin >> cen_1;
 		f2->SetParameter(1, cen_1);
 		f2->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		
 		std::cout << "Centroid 2:" << std::endl;
 		std::cin >> cen_2;
 		f2->SetParameter(4, cen_2);
 		f2->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		
 		//setting up the fitting of the standard deviation:
 		f2->SetParLimits(2,0., up_x-low_x);
 		f2->SetParLimits(5,0., up_x-low_x);
 		f2->SetParameter(2, FWHM_est/2.35);
 		f2->SetParameter(5, FWHM_est/2.35);
+		
 		h1->Fit("f2", "IM", "", low_x, up_x);
 		break;
 		}
 	case 3: {
 		TF1 *f3 = new TF1("f3", "gausn(0) + gausn(3) + gausn(6) + pol1(9)", low_x, up_x);
-		f3->SetParNames("1 Counts", "1 Centroid" ,"1 Sigma", "2 Counts", "2 Centroid" ,"2 Sigma", "3 Counts", "3 Centroid" ,"3 Sigma", "Intercept","Slope");
+		f3->SetParName(0 , "1 Counts");
+		f3->SetParName(1 , "1 Centroid");
+		f3->SetParName(2 , "1 Sigma");
+		f3->SetParName(3 , "2 Counts");
+		f3->SetParName(4 , "2 Centroid");
+		f3->SetParName(5 , "2 Sigma");
+		f3->SetParName(6 , "3 Counts");
+		f3->SetParName(7 , "3 Centroid");
+		f3->SetParName(8 , "3 Sigma");
+		f3->SetParName(9 , "Intercept");
+		f3->SetParName(10, "Slope");
 		//Assuming tha both centroids are seen in the selected region:
 		f3->SetParLimits(1, low_x, up_x);
 		f3->SetParLimits(4, low_x, up_x);
@@ -108,10 +132,12 @@ void FitData(){
 		std::cin >> cen_1;
 		f3->SetParameter(1, cen_1);
 		f3->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		
 		std::cout << "Centroid 2:" << std::endl;
 		std::cin >> cen_2;
 		f3->SetParameter(4, cen_2);
 		f3->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		
 		std::cout << "Centroid 3:" << std::endl;
 		std::cin >> cen_3;
 		f3->SetParameter(7, cen_3);
@@ -123,23 +149,24 @@ void FitData(){
 		f3->SetParameter(2, FWHM_est/2.35);
 		f3->SetParameter(5, FWHM_est/2.35);
 		f3->SetParameter(8, FWHM_est/2.35);
+		
 		h1->Fit("f3", "IM", "", low_x, up_x);
 		break;
 		}
 	case 4: {
 		TF1 *f4 = new TF1("f4", "gausn(0) + gausn(3) + gausn(6) + gausn(9) + pol1(12)", low_x, up_x);
-		f4->SetParName(0, "1 Counts");
-		f4->SetParName(1, "1 Centroid");
-		f4->SetParName(2 ,"1 Sigma");
-		f4->SetParName(3, "2 Counts");
-		f4->SetParName(4, "2 Centroid");
-		f4->SetParName(5 ,"2 Sigma");
-		f4->SetParName(6, "3 Counts");
-		f4->SetParName(7, "3 Centroid");
-		f4->SetParName(8 ,"3 Sigma");
-		f4->SetParName(9,  "4 Counts");
+		f4->SetParName(0 , "1 Counts");
+		f4->SetParName(1 , "1 Centroid");
+		f4->SetParName(2 , "1 Sigma");
+		f4->SetParName(3 , "2 Counts");
+		f4->SetParName(4 , "2 Centroid");
+		f4->SetParName(5 , "2 Sigma");
+		f4->SetParName(6 , "3 Counts");
+		f4->SetParName(7 , "3 Centroid");
+		f4->SetParName(8 , "3 Sigma");
+		f4->SetParName(9 , "4 Counts");
 		f4->SetParName(10, "4 Centroid");
-		f4->SetParName(11 , "4 Sigma");
+		f4->SetParName(11, "4 Sigma");
 		f4->SetParName(12, "Intercept");
 		f4->SetParName(13, "Slope");
 		//Assuming tha both centroids are seen in the selected region:
@@ -152,18 +179,22 @@ void FitData(){
 		std::cin >> cen_1;
 		f4->SetParameter(1, cen_1);
 		f4->SetParameter(0, h1->GetBinContent(int(cen_1)));
+		
 		std::cout << "Centroid 2:" << std::endl;
 		std::cin >> cen_2;
 		f4->SetParameter(4, cen_2);
 		f4->SetParameter(3, h1->GetBinContent(int(cen_2)));
+		
 		std::cout << "Centroid 3:" << std::endl;
 		std::cin >> cen_3;
 		f4->SetParameter(7, cen_3);
 		f4->SetParameter(6, h1->GetBinContent(int(cen_3)));
+		
 		std::cout << "Centroid 4" << std::endl;
 		std::cin >> cen_4;
 		f4->SetParameter(10, cen_4);
 		f4->SetParameter(9, h1->GetBinContent(int(cen_4)));
+		
 		//setting up the fitting of the standard deviation:
 		f4->SetParLimits(2,0., up_x-low_x);
 		f4->SetParLimits(5,0., up_x-low_x);
@@ -174,12 +205,8 @@ void FitData(){
 		f4->SetParameter(8, FWHM_est/2.35);
 		f4->SetParameter(11, FWHM_est/2.35);
 		h1->Fit("f4", "IM", "", low_x, up_x);
-	
-	
 		break;
 		}
-		
-	
 	}
 	
 	h1->Draw("Same");
@@ -191,6 +218,17 @@ void Zoom(int low_x, int up_x){
 //This function re-adjust the display window to specifed channel numbers.
 	h1->GetXaxis()->SetRangeUser(low_x, up_x);
 	h1->Draw("Same");
+}
+
+void Zoom(){
+	int low_x, up_x;
+	std::cout << "Enter the lower boundry channel:" << std::endl;
+	std::cin >> low_x;
+	std::cout << "Enter the upper boundry channel:" << std::endl;
+	std::cin >> up_x;
+	h1->GetXaxis()->SetRangeUser(low_x, up_x);
+	h1->Draw("Same");
+	
 }
 
 void ZoomOut(){
