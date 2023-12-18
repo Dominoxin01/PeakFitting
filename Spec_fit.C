5c0ed21bf2787416becf437313622a40d0427a81
Dominik Stajkowski <d.stajkowski.1@pgr.bham.ac.uk>
2023-12-18 12:14:18 +0000

    Added a print out of Chi2 and ndf when carrying out fitting for one spectrum.

--- Spec_fit.C
+++ Spec_fit.C
@@ -73,6 +73,9 @@ void FitData(){
     		//assuming that the standard deviation is half the selected width (may be sub-optimal:)
     		f1->SetParameter(2, 0.5*(up_x-low_x)/2.35);
     		//std::cout << up_x-low_x << std::endl;
+    		
+    		f1 -> GetChisquare();
+		f1 -> GetNDF();
     		h1->Fit("f1", "IM", "", low_x, up_x);
 		break;
 		}
@@ -107,6 +110,8 @@ void FitData(){
 		f2->SetParameter(2, FWHM_est/2.35);
 		f2->SetParameter(5, FWHM_est/2.35);
 		
+		f2 -> GetChisquare();
+		f2 -> GetNDF();
 		h1->Fit("f2", "IM", "", low_x, up_x);
 		break;
 		}
@@ -150,6 +155,8 @@ void FitData(){
 		f3->SetParameter(5, FWHM_est/2.35);
 		f3->SetParameter(8, FWHM_est/2.35);
 		
+		f3 -> GetChisquare();
+		f3 -> GetNDF();
 		h1->Fit("f3", "IM", "", low_x, up_x);
 		break;
 		}
@@ -204,10 +211,15 @@ void FitData(){
 		f4->SetParameter(5, FWHM_est/2.35);
 		f4->SetParameter(8, FWHM_est/2.35);
 		f4->SetParameter(11, FWHM_est/2.35);
+		
+		f4 -> GetChisquare();
+		f4 -> GetNDF();
 		h1->Fit("f4", "IM", "", low_x, up_x);
+
 		break;
 		}
 	}
+	
 	
 	h1->Draw("Same");
 }
